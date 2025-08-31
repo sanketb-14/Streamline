7fb382a3784c1d256bc54044bf13b771c7d04d4e
sanket <sanketbarapatre999@gmail.com>
2025-09-01 05:02:43 +0530

    ready for production

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,4 +1,9 @@
-import app from "../app.js";
+// api/index.js
 import serverless from "serverless-http";
+import app from "../app.js";
+import connectDB from "../config/database.js";
+
+// Ensure DB connection once (not inside handler)
+await connectDB();
 
 export default serverless(app);
